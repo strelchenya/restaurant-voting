13be1e8e9649320f7752f57c75450c44f037bf70
Igor Strelchenya <just.sicarius@gmail.com>
2021-12-27 20:21:02 +0300

    Create transfer object

---
+++ src/main/java/com/strelchenya/restaurantvoting/to/UserTo.java
@@ -0,0 +1,32 @@
+package com.strelchenya.restaurantvoting.to;
+
+import com.strelchenya.restaurantvoting.model.HasIdAndEmail;
+import com.strelchenya.restaurantvoting.util.validation.NoHtml;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.Value;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class UserTo extends NamedTo implements HasIdAndEmail {
+    @Email
+    @NotBlank
+    @Size(max = 100)
+    @NoHtml  // https://stackoverflow.com/questions/17480809
+    String email;
+
+    @NotBlank
+    @Size(min = 5, max = 32)
+    String password;
+
+    public UserTo(Integer id, String name, String email, String password) {
+        super(id, name);
+        this.email = email;
+        this.password = password;
+    }
+}
