88d94d83bc0dc7703cd2fa8b0b8dd8acd8bf4d2b
Igor Strelchenya <just.sicarius@gmail.com>
2021-12-29 02:15:49 +0300

    Update initial data and database schema

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,13 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM vote;
+DELETE
+FROM users;
+DELETE
+FROM restaurant;
+DELETE
+FROM dish;
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', '{noop}password'),
@@ -9,3 +17,21 @@ INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 1),
        ('ADMIN', 2),
        ('USER', 2);
+
+INSERT INTO restaurant (title)
+VALUES ('Just restaurant'),
+       ('Burger Empire'),
+       ('Eatery');
+
+INSERT INTO vote (local_date, restaurant_id, user_id)
+VALUES ('2021-12-12', 1, 1),
+       ('2021-12-12', 2, 2),
+       ('2021-12-13', 2, 1),
+       ('2021-12-13', 3, 2),
+       ('2021-12-14', 3, 1),
+       ('2021-12-14', 1, 2);
+
+INSERT INTO dish (local_date, price, title, restaurant_id)
+VALUES ('2021-12-10', 211, 'Just Lunch', 1),
+       ('2021-12-10', 3425, 'Coca-Cola', 3),
+       (now(), 123, 'Burger', 2);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,27 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS vote;
+DROP TABLE IF EXISTS dish;
+DROP TABLE IF EXISTS restaurant;
 DROP TABLE IF EXISTS users;
+
+CREATE TABLE restaurant
+(
+    id    INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL
+);
+CREATE UNIQUE INDEX restaurants_unique_title_idx ON restaurant (title);
+
+CREATE TABLE dish
+(
+    id            INTEGER AUTO_INCREMENT PRIMARY KEY,
+    local_date    DATE DEFAULT now() NOT NULL,
+    price         INTEGER            NOT NULL,
+    title         VARCHAR(255)       NOT NULL,
+    restaurant_id INTEGER            NOT NULL,
+    FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE,
+    CONSTRAINT dish_unique_restaurant_title_date_idx UNIQUE (restaurant_id, title, local_date)
+);
+CREATE INDEX dish_date_idx ON dish (local_date);
 
 CREATE TABLE users
 (
@@ -8,10 +30,9 @@ CREATE TABLE users
     email      VARCHAR(255)            NOT NULL,
     password   VARCHAR(255)            NOT NULL,
     enabled    BOOLEAN   DEFAULT TRUE  NOT NULL,
-    registered TIMESTAMP DEFAULT now() NOT NULL
+    registered TIMESTAMP DEFAULT NOW() NOT NULL
 );
 CREATE UNIQUE INDEX user_unique_email_idx ON users (email);
-
 
 CREATE TABLE user_roles
 (
@@ -20,3 +41,14 @@ CREATE TABLE user_roles
     CONSTRAINT user_role_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE vote
+(
+    id            INTEGER AUTO_INCREMENT PRIMARY KEY,
+    local_date    DATE DEFAULT NOW() NOT NULL,
+    restaurant_id INTEGER            NOT NULL,
+    user_id       INTEGER            NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX vote_unique_user_date_idx ON vote (user_id, local_date);
