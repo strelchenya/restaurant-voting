211f172844c40e64a2b7f5e26b3e0aa514843454
Igor Strelchenya <just.sicarius@gmail.com>
2022-01-05 12:43:14 +0300

    Update vote service
    1. Add logging
    2. Code refactoring

--- src/main/java/com/strelchenya/restaurantvoting/service/VoteService.java
+++ src/main/java/com/strelchenya/restaurantvoting/service/VoteService.java
@@ -7,6 +7,7 @@ import com.strelchenya.restaurantvoting.repository.UserRepository;
 import com.strelchenya.restaurantvoting.repository.VoteRepository;
 import com.strelchenya.restaurantvoting.to.VoteTo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
@@ -14,8 +15,11 @@ import org.springframework.util.Assert;
 import java.time.LocalDate;
 import java.util.List;
 
+import static com.strelchenya.restaurantvoting.util.ToUtil.asVote;
+import static com.strelchenya.restaurantvoting.util.ToUtil.asVoteTo;
 import static com.strelchenya.restaurantvoting.util.validation.ValidationUtil.*;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
@@ -44,21 +48,34 @@ public class VoteService {
     }
 
     @Transactional
-    public Vote create(Vote vote, int userId) {
-        Assert.notNull(vote, "vote must not be null!");
-        checkNew(vote);
-        vote.setUser(userRepository.getById(userId));
-        return voteRepository.save(vote);
+    public VoteTo create(VoteTo voteTo, int userId) {
+        Assert.notNull(voteTo, "vote must not be null!");
+        checkNew(voteTo);
+        Vote vote = asVote(voteTo);
+        vote.setUser(userRepository.findById(userId).orElseThrow(() ->
+                new NotFoundException("not found user by Id " + userId)));
+        vote.setRestaurant(restaurantRepository.findById(voteTo.getRestaurantId()).orElseThrow(() ->
+                new NotFoundException("not found restaurant by id " + voteTo.getRestaurantId())));
+        log.info("convert VoteTo to Vote: {}", vote);
+        return asVoteTo(voteRepository.save(vote));
     }
 
     @Transactional
-    public void update(Vote vote, int userId) {
-        Assert.notNull(vote, "vote must not be null!");
-        assureIdConsistent(vote, vote.id());
-        userRepository.findById(userId).orElseThrow(() ->
-                new NotFoundException("not found user by Id " + userId));
-        restaurantRepository.findById(vote.getRestaurant().id()).orElseThrow(() ->
-                new NotFoundException("not found restaurant by id " + vote.getRestaurant().id()));
-        voteRepository.save(vote);
+    public void update(VoteTo voteTo, int userId) {
+        Assert.notNull(voteTo, "vote must not be null!");
+        assureIdConsistent(voteTo, voteTo.id());
+
+        LocalDate checkDate = voteRepository.checkDate(voteTo.getId(), userId).orElseThrow(() ->
+                new NotFoundException("not found localDate by Id " + voteTo.getId()));
+
+        if (checkDate.equals(voteTo.getLocalDate())) {
+            Vote vote = new Vote(voteTo.getId(), voteTo.getLocalDate(), voteTo.getLocalTime());
+            vote.setUser(userRepository.findById(userId).orElseThrow(() ->
+                    new NotFoundException("not found user by Id " + userId)));
+            vote.setRestaurant(restaurantRepository.findById(voteTo.getRestaurantId()).orElseThrow(() ->
+                    new NotFoundException("not found restaurant by id " + voteTo.getRestaurantId())));
+            log.info("convert VoteTo to Vote: {}", vote);
+            voteRepository.save(vote);
+        }
     }
 }
