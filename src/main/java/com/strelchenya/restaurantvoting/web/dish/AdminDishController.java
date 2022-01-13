768b51d7afb3ed7202b5ed0267fe0c4b67c62fba
Igor Strelchenya <just.sicarius@gmail.com>
2022-01-13 22:56:30 +0300

    Update swagger for AdminDishController

--- src/main/java/com/strelchenya/restaurantvoting/web/dish/AdminDishController.java
+++ src/main/java/com/strelchenya/restaurantvoting/web/dish/AdminDishController.java
@@ -2,6 +2,8 @@ package com.strelchenya.restaurantvoting.web.dish;
 
 import com.strelchenya.restaurantvoting.model.Dish;
 import com.strelchenya.restaurantvoting.service.DishService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -21,11 +23,13 @@ import static com.strelchenya.restaurantvoting.web.restaurant.AdminRestaurantCon
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = ADMIN_RESTAURANT_REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Dish Controller", description = "Controller for editing dishes by admin.")
 public class AdminDishController {
     public static final String DISHES_REST_URL = "/{restaurantId}/dishes";
 
     private final DishService dishService;
 
+    @Operation(summary = "Creating a dish", description = "Creating a dish for a restaurant.")
     @PostMapping(value = DISHES_REST_URL, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Dish> create(@Valid @RequestBody Dish dish, @PathVariable int restaurantId) {
         log.info("create dish {} for restaurant {}", dish, restaurantId);
@@ -38,6 +42,7 @@ public class AdminDishController {
         return ResponseEntity.created(uriOfNewResource).body(created);
     }
 
+    @Operation(summary = "Dish update", description = "Renovation of the restaurant dishes.")
     @PutMapping(value = DISHES_REST_URL + "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@Valid @RequestBody Dish dish, @PathVariable int id, @PathVariable int restaurantId) {
@@ -46,6 +51,7 @@ public class AdminDishController {
         dishService.update(dish, id, restaurantId);
     }
 
+    @Operation(summary = "Deleting a dish", description = "Deleting a dish in a specific restaurant.")
     @DeleteMapping(value = DISHES_REST_URL + "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable int restaurantId, @PathVariable int id) {
