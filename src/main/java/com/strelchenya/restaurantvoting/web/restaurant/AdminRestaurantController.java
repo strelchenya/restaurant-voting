5773674ab0458d4e53421d149056980db3da6038
Igor Strelchenya <just.sicarius@gmail.com>
2022-01-13 23:37:54 +0300

    Update swagger for Restaurant Controllers

--- src/main/java/com/strelchenya/restaurantvoting/web/restaurant/AdminRestaurantController.java
+++ src/main/java/com/strelchenya/restaurantvoting/web/restaurant/AdminRestaurantController.java
@@ -2,6 +2,8 @@ package com.strelchenya.restaurantvoting.web.restaurant;
 
 import com.strelchenya.restaurantvoting.model.Restaurant;
 import com.strelchenya.restaurantvoting.service.RestaurantService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -19,11 +21,13 @@ import static com.strelchenya.restaurantvoting.web.restaurant.AdminRestaurantCon
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = ADMIN_RESTAURANT_REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Admin Restaurant Controller", description = "Controller for restaurant management by administrator.")
 public class AdminRestaurantController {
     public static final String ADMIN_RESTAURANT_REST_URL = "/api/v1/admin/restaurants";
 
     private final RestaurantService restaurantService;
 
+    @Operation(summary = "Creation of a restaurant", description = "Creation of a new restaurant by the administrator.")
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Restaurant> create(@Valid @RequestBody Restaurant restaurant) {
         log.info("create {}", restaurant);
@@ -34,6 +38,7 @@ public class AdminRestaurantController {
         return ResponseEntity.created(uriOfNewResource).body(created);
     }
 
+    @Operation(summary = "Restaurant update", description = "Restaurant update by administrator.")
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@Valid @RequestBody Restaurant restaurant, @PathVariable int id) {
@@ -41,6 +46,7 @@ public class AdminRestaurantController {
         restaurantService.update(restaurant, id);
     }
 
+    @Operation(summary = "Deleting a restaurant", description = "Removal by the restaurant administrator.")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable int id) {
--- src/main/java/com/strelchenya/restaurantvoting/web/restaurant/RestaurantController.java
+++ src/main/java/com/strelchenya/restaurantvoting/web/restaurant/RestaurantController.java
@@ -2,6 +2,8 @@ package com.strelchenya.restaurantvoting.web.restaurant;
 
 import com.strelchenya.restaurantvoting.service.RestaurantService;
 import com.strelchenya.restaurantvoting.to.RestaurantTo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -15,17 +17,23 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = RestaurantController.RESTAURANTS_REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "User Restaurant Controller", description = "Getting a restaurant by a user with menu votes.")
 public class RestaurantController {
     public static final String RESTAURANTS_REST_URL = "/api/v1/restaurants";
 
     private final RestaurantService restaurantService;
 
+    @Operation(summary = "Get a restaurant", description = "Get a restaurant with all the menu votes of all time.")
     @GetMapping("/{id}")
     public RestaurantTo getById(@PathVariable int id) {
         log.info("get restaurant by id {}", id);
         return restaurantService.getById(id);
     }
 
+    @Operation(
+            summary = "Get a restaurant with votes",
+            description = "Get a restaurant with the number of votes for a menu for a given day."
+    )
     @GetMapping("/{id}" + "/by")
     public RestaurantTo getByIdAndLocalDate(@PathVariable int id,
                                             @NotNull @RequestParam(value = "local-date") LocalDate localDate) {
@@ -33,6 +41,10 @@ public class RestaurantController {
         return restaurantService.getByIdAndLocalDate(id, localDate);
     }
 
+    @Operation(
+            summary = "Get all restaurants",
+            description = "Get all restaurants with the number of votes on a given day."
+    )
     @GetMapping("/by")
     public List<RestaurantTo> getAllByDate(@NotNull @RequestParam(value = "local-date") LocalDate localDate) {
         log.info("get all restaurantTos by date {}", localDate);
