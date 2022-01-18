c555c11fcfd449c432a0e249a0fd9bc56a9886d3
Igor Strelchenya <just.sicarius@gmail.com>
2022-01-18 22:53:56 +0300

    Fix: removed validation annotation

--- src/main/java/com/strelchenya/restaurantvoting/to/DishTo.java
+++ src/main/java/com/strelchenya/restaurantvoting/to/DishTo.java
@@ -1,14 +1,9 @@
 package com.strelchenya.restaurantvoting.to;
 
-import com.strelchenya.restaurantvoting.util.validation.NoHtml;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import lombok.Value;
-import org.hibernate.validator.constraints.Range;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Value
@@ -16,23 +11,15 @@ import java.time.LocalDate;
 @ToString(callSuper = true)
 public class DishTo extends BaseTo {
 
-    @Size(min = 2, max = 255)
-    @NotBlank
-    @NoHtml
     String title;
 
-    @Range(min = 10, max = 10000)
-    @NotNull
-    Integer price;
+    int price;
 
-    @NotNull
     LocalDate localDate;
 
-    @Range(min = 1)
-    @NotNull
     Integer restaurantId;
 
-    public DishTo(Integer id, String title, Integer price, LocalDate localDate, Integer restaurantId) {
+    public DishTo(Integer id, String title, int price, LocalDate localDate, Integer restaurantId) {
         super(id);
         this.title = title;
         this.price = price;
--- src/main/java/com/strelchenya/restaurantvoting/to/RestaurantTo.java
+++ src/main/java/com/strelchenya/restaurantvoting/to/RestaurantTo.java
@@ -1,27 +1,16 @@
 package com.strelchenya.restaurantvoting.to;
 
-import com.strelchenya.restaurantvoting.util.validation.NoHtml;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import lombok.Value;
-import org.hibernate.validator.constraints.Range;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Value
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public class RestaurantTo extends BaseTo {
 
-    @Size(min = 2, max = 255)
-    @NotBlank
-    @NoHtml
     String title;
 
-    @Range(min = 0)
-    @NotNull
     Long voices;
 
     public RestaurantTo(int id, String title, Long voices) {
