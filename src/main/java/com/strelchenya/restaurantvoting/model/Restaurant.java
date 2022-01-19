4069ff5dd2968fbdf0fb04e2764e722c77cfaa03
Igor Strelchenya <just.sicarius@gmail.com>
2022-01-19 21:27:01 +0300

    Removed unused fields

--- src/main/java/com/strelchenya/restaurantvoting/model/Restaurant.java
+++ src/main/java/com/strelchenya/restaurantvoting/model/Restaurant.java
@@ -1,20 +1,17 @@
 package com.strelchenya.restaurantvoting.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.strelchenya.restaurantvoting.util.validation.NoHtml;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.List;
 
 @Entity
 @Setter
@@ -30,17 +27,6 @@ public class Restaurant extends BaseEntity {
     @NoHtml
     private String title;
 
-    @JsonManagedReference
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "restaurant")
-    @Fetch(value = FetchMode.SUBSELECT)
-    private List<MenuItem> menu;
-
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "restaurant")
-    @Fetch(value = FetchMode.SUBSELECT)
-    private List<Vote> votes;
-
     public Restaurant(Restaurant restaurant) {
         this(restaurant.getId(), restaurant.getTitle());
     }
@@ -48,13 +34,6 @@ public class Restaurant extends BaseEntity {
     public Restaurant(Integer id, String title) {
         super(id);
         this.title = title;
-    }
-
-    public Restaurant(Integer id, String title, List<MenuItem> menu, List<Vote> votes) {
-        super(id);
-        this.title = title;
-        this.menu = menu;
-        this.votes = votes;
     }
 
     @Override
