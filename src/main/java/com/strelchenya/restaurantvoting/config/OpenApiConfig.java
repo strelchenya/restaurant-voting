a80a038d886780eecca0dad7e09573c5b432385d
Igor Strelchenya <just.sicarius@gmail.com>
2022-01-22 01:19:08 +0300

    Fix redefining LocalTime representations to OpenAPI

--- src/main/java/com/strelchenya/restaurantvoting/config/OpenApiConfig.java
+++ src/main/java/com/strelchenya/restaurantvoting/config/OpenApiConfig.java
@@ -6,9 +6,14 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.media.Schema;
 import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 //https://sabljakovich.medium.com/adding-basic-auth-authorization-option-to-openapi-swagger-documentation-java-spring-95abbede27e9
@@ -28,6 +33,12 @@ import org.springframework.context.annotation.Configuration;
         security = @SecurityRequirement(name = "basicAuth")
 )
 public class OpenApiConfig {
+
+    static {
+        var schema = new Schema<LocalTime>();
+        schema.example(LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+        SpringDocUtils.getConfig().replaceWithSchema(LocalTime.class, schema);
+    }
 
     @Bean
     public GroupedOpenApi api() {
